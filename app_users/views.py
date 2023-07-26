68abb5fa4fd9bc3a7e2e0dc0260d118b991573bc
Koltun <olha.koltun@gmail.com>
2023-07-26 09:29:07 +0300

    Initial commit

---
+++ app_users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def main(request):
+    title = 'Main Page'
+
+    return render(request,
+                  'app_users/main.html',
+                  {'title': title})
+
+
+def user_info(request):
+    userinfo = {
+        'username': 'Olha',  # Put your name here
+        'country': 'Ukraine',  # Put your country here
+    }
+    context = {'userinfo': userinfo,
+               'title': 'User Info Page'}
+
+    return render(request,
+                  'app_users/user_info.html',
+                  context)
